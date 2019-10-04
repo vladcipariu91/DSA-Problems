1899acb5b5341f66cdd8a33d922cebedc78ad45e
Vlad <vladcipariu@gmail.com>
2019-10-04 15:33:59 +0100

    implements the lru cache (1st exam problem, chapter2)

---
+++ chapter_2/exam/lru_cache.py
@@ -0,0 +1,207 @@
+import unittest
+
+
+class LRU_Cache(object):
+
+    def __init__(self, capacity):
+        self.__capacity = capacity
+        self.__doublyLinkedList = DoublyLinkedList()
+        self.__node_map = {}
+
+    def get(self, key):
+        # Retrieve item from provided key. Return -1 if nonexistent.
+        found_node = self.__find_node(key)
+        if found_node is None:
+            return -1
+
+        self.__set_most_recently_used(found_node)
+        return found_node.value
+
+    def set(self, key, value):
+        if self.__doublyLinkedList.count >= self.__capacity:
+            removed_node = self.__doublyLinkedList.tail
+            self.__doublyLinkedList.remove_last()
+            if removed_node is not None:
+                self.__node_map.pop(removed_node.key)
+
+        self.__doublyLinkedList.add_first(key, value)
+        added_node = self.__doublyLinkedList.head
+        self.__node_map[added_node.key] = added_node
+
+    def __find_node(self, key):
+        return self.__node_map.get(key)
+
+    def __set_most_recently_used(self, current_node):
+        if current_node == self.__doublyLinkedList.head:
+            return
+
+        next_node = None
+        if current_node == self.__doublyLinkedList.tail:
+            self.__doublyLinkedList.tail = current_node.previous
+        else:
+            next_node = current_node.next
+            next_node.previous = current_node.previous
+
+        current_node.previous.next = next_node
+
+        current_node.previous = None
+        current_node.next = self.__doublyLinkedList.head
+        self.__doublyLinkedList.head.previous = current_node
+        self.__doublyLinkedList.head = current_node
+
+    # test helper function
+    def to_list(self):
+        return self.__doublyLinkedList.to_list()
+
+    # test helper function
+    def map_size(self):
+        return len(self.__node_map)
+
+
+class Node(object):
+
+    def __init__(self, key, value):
+        self.previous = None
+        self.next = None
+        self.key = key
+        self.value = value
+
+    def __repr__(self):
+        return "Node( key: {}, value: {} ) ".format(self.key, self.value)
+
+
+class DoublyLinkedList(object):
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.count = 0
+
+    def add_first(self, key, value):
+        new_node = Node(key, value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = self.head
+        else:
+            new_node.next = self.head
+            self.head.previous = new_node
+            self.head = self.head.previous
+
+        self.count += 1
+
+    def remove_last(self):
+        if self.head is None:
+            return
+
+        self.tail.previous.next = None
+        self.tail = self.tail.previous
+        self.tail.next = None
+
+        self.count -= 1
+
+    def size(self):
+        return self.count
+
+    def to_list(self):
+        current = self.head
+        result = []
+        while current is not None:
+            result.append(current.value)
+            current = current.next
+
+        return result
+
+    def __repr__(self):
+        current = self.head
+        print_result = ""
+        while current is not None:
+            print_result += str(current)
+            current = current.next
+
+        return print_result
+
+
+# Testing
+class TestLruCache(unittest.TestCase):
+
+    def setUp(self):
+        self.lru_cache = LRU_Cache(5)
+
+    def tearDown(self):
+        self.lru_cache = None
+
+    def test_cache_hit_first(self):
+        self.lru_cache.set(1, 1)
+        self.lru_cache.set(2, 2)
+        self.lru_cache.set(3, 3)
+
+        expected_list = [3, 2, 1]
+        expected_result = 3
+
+        item = self.lru_cache.get(3)
+
+        self.assertEqual(self.lru_cache.to_list(), expected_list)
+        self.assertEqual(item, expected_result)
+        self.assertEqual(self.lru_cache.map_size(), 3)
+
+    def test_cache_hit_middle(self):
+        self.lru_cache.set(1, 1)
+        self.lru_cache.set(2, 2)
+        self.lru_cache.set(3, 3)
+
+        expected_list = [2, 3, 1]
+        expected_result = 2
+
+        item = self.lru_cache.get(2)
+
+        self.assertEqual(self.lru_cache.to_list(), expected_list)
+        self.assertEqual(item, expected_result)
+        self.assertEqual(self.lru_cache.map_size(), 3)
+
+    def test_cache_hit_last(self):
+        self.lru_cache.set(1, 1)
+        self.lru_cache.set(2, 2)
+        self.lru_cache.set(3, 3)
+
+        expected_list = [1, 3, 2]
+        expected_result = 1
+
+        item = self.lru_cache.get(1)
+
+        self.assertEqual(self.lru_cache.to_list(), expected_list)
+        self.assertEqual(item, expected_result)
+        self.assertEqual(self.lru_cache.map_size(), 3)
+
+    def test_cache_miss(self):
+        self.lru_cache.set(1, 1)
+        self.lru_cache.set(2, 2)
+        self.lru_cache.set(3, 3)
+
+        expected_list = [3, 2, 1]
+        expected_result = -1
+
+        item = self.lru_cache.get(4)
+
+        self.assertEqual(self.lru_cache.to_list(), expected_list)
+        self.assertEqual(item, expected_result)
+        self.assertEqual(self.lru_cache.map_size(), 3)
+
+    def test_set_with_when_cache_is_full(self):
+        self.lru_cache.set(1, 1)
+        self.lru_cache.set(2, 2)
+        self.lru_cache.set(3, 3)
+        self.lru_cache.set(4, 4)
+        self.lru_cache.set(5, 5)
+
+        expected_list = [5, 4, 3, 2, 1]
+        self.assertEqual(self.lru_cache.to_list(), expected_list)
+        self.assertEqual(self.lru_cache.map_size(), 5)
+
+        self.lru_cache.set(6, 6)
+        expected_list = [6, 5, 4, 3, 2]
+        self.assertEqual(self.lru_cache.to_list(), expected_list)
+        self.assertEqual(self.lru_cache.map_size(), 5)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- testing/multiply_string.py
+++ testing/multiply_string.py
@@ -8,11 +8,13 @@ def nonDivisibleSubset(k, s):
     subset = set()
     for i in range(0, l_arr - 1):
         for j in range(i + 1, l_arr):
-            if (arr[i] + arr[j]) % k == 0:
+            if (arr[i] + arr[j]) % k != 0:
                 subset.add(arr[i])
                 subset.add(arr[j])
 
+    return subset
 
 
 
-nonDivisibleSubset(7, [278, 576, 496, 727, 410, 124, 338, 149, 209, 702, 282, 718, 771, 575, 436])
+
+print(nonDivisibleSubset(3, [1, 4, 7, 2]))
