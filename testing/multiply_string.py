d8b96e8eb3c3cb62d57dd5ec0d52dce013580f86
vlad <vladcipariu@gmail.com>
2019-10-04 08:25:49 +0100

    tests strings and combination generation

--- testing/multiply_string.py
+++ testing/multiply_string.py
@@ -1,11 +1,18 @@
 a = "b"
 b = 3 * a
-print(b)
 
 
-def flippingBits(n):
-    str = 32 * "1"
-    return int(str, 2) - n
+def nonDivisibleSubset(k, s):
+    arr = list(s)
+    l_arr = len(arr)
+    subset = set()
+    for i in range(0, l_arr - 1):
+        for j in range(i + 1, l_arr):
+            if (arr[i] + arr[j]) % k == 0:
+                subset.add(arr[i])
+                subset.add(arr[j])
 
 
-print(flippingBits(9))
+
+
+nonDivisibleSubset(7, [278, 576, 496, 727, 410, 124, 338, 149, 209, 702, 282, 718, 771, 575, 436])
