2e91c013acf4d58c7f2811cffb54113709766ff5
vlad <vladcipariu@gmail.com>
2019-11-01 09:42:55 +0000

    creates the skeleton class for the priority queue.

---
+++ chapter_3/heap/prority_queue.py
@@ -0,0 +1,17 @@
+class PriorityQueue(object):
+
+    def insert(self):
+        pass
+
+    def remove(self):
+        pass
+
+    # returns the element at the front of the queue
+    def front(self):
+        pass
+
+    def size(self):
+        pass
+
+    def is_empty(self):
+        pass
